b45daeb9dfb0d42dfd7292f35c078d73bd4009bd
Denis Kuznecov <vdv-den@mail.ru>
2022-04-23 11:43:19 +0300

    helpers

---
+++ js/helpers.js
@@ -0,0 +1,24 @@
+/* helpers.js */
+
+/** @param {string} name */
+export function getNumberOfChars(name) {
+    // your code
+  return (name.length)
+  };
+  
+  
+  /** @param {string} name */
+  export function getLower(name) {
+    // your code
+    return name.toLowerCase()
+  
+  };
+  
+  /** @param {string} name */
+  export function getUpper(name) {
+    // your code
+   return name.toUpperCase()
+
+  };
+
+  
